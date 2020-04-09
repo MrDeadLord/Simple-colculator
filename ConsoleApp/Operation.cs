9e82f3da330bfee22fc59b3b18be3f2ec970e533
MrDeadLord <madeadlord@gmail.com>
2020-04-09 12:40:38 +0300

    just open ConsoleApp.sln in Visual studio

---
+++ ConsoleApp/Operation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Operation
+    {
+        public string Sign()
+        {
+            string sign;
+
+            do
+            {
+                Console.WriteLine("Введите знак операции(+, -, *, /):");
+                sign = Console.ReadLine();
+
+                if (sign != "+" && sign != "-" && sign != "*" && sign != "/")
+                {
+                    Console.WriteLine("Unknown sumbol(s) :(");
+                }
+                else
+                {
+                    Console.WriteLine("You entered: " + sign);
+                    break;
+                }
+            } while (sign != "+" || sign != "-" || sign != "*" || sign != "/");  //Проверка верности введенного знака
+
+            return sign;
+        }
+
+        public void Plus(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine("------------------");
+            Console.WriteLine("A + B = " + result);
+
+        }
+
+        public void Minus(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine("------------------");
+            Console.WriteLine("A - B = " + result);
+        }
+
+        public void Multiply(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine("------------------");
+            Console.WriteLine("A * B = " + result);
+        }
+
+        public void Division(int a, int b)
+        {
+            float result = a / b;
+            Console.WriteLine("------------------");
+            Console.WriteLine("A / B = " + result);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class ConsoleApp
+    {
+        static void Main()
+        {
+            var op = new Operation();
+
+            while (true)
+            {
+                var sign = op.Sign();
+
+                string input;   //Вводимое поьзователем число
+                int a;      //Для определения является ли введенное число числом.
+
+                do
+                {
+                    Console.WriteLine("Введите число А:");
+                    input = (Console.ReadLine());
+                    if (int.TryParse(input, out a))
+                    {
+                        break;
+                    }
+                } while (!int.TryParse(input, out a));     //Ввод чила А, пока оно не будет числом
+
+                int b;    //Для определения является ли введенное число числом.
+                do
+                {
+                    Console.WriteLine("Введите число B:");
+                    input = (Console.ReadLine());
+                    if (int.TryParse(input, out b))
+                    {
+                        break;
+                    }
+                } while (!int.TryParse(input, out b));     //Ввод чила В, пока оно не будет числом
+
+                switch (sign)
+                {
+                    case "+":
+                        op.Plus(a, b);
+                        break;
+                    case "-":
+                        op.Minus(a, b);
+                        break;
+                    case "*":
+                        op.Multiply(a, b);
+                        break;
+                    default:
+                        op.Division(a, b);
+                        break;
+                }
+
+                Console.WriteLine("Exit? y/n...");
+                input = Console.ReadLine();
+                if (input == "y")
+                {
+                    Console.ReadKey();
+                    break;
+                }
+
+                Console.Clear();
+            }
+        }
+    }
+}
